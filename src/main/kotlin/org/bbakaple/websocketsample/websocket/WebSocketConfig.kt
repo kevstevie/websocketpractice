95266f4a59f0a07f2466fa0218b1209306806c13
kevstevie <jianna442@gmail.com>
2024-08-24 00:42:33 +0900

    refactor: config 수정

--- src/main/kotlin/org/bbakaple/websocketsample/websocket/WebSocketConfig.kt
+++ src/main/kotlin/org/bbakaple/websocketsample/websocket/WebSocketConfig.kt
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 class WebSocketConfig : WebSocketMessageBrokerConfigurer {
 
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
-        registry.addEndpoint("/chat").withSockJS()
+        registry.addEndpoint("/ws").withSockJS()
     }
 
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
