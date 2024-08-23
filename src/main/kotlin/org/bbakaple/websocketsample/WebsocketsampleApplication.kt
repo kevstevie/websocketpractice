8f16402ccf78f82d9fd67610b28c48ccb55d6509
kevstevie <jianna442@gmail.com>
2024-08-23 16:24:59 +0900

    init

---
+++ src/main/kotlin/org/bbakaple/websocketsample/WebsocketsampleApplication.kt
@@ -0,0 +1,11 @@
+package org.bbakaple.websocketsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebsocketsampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebsocketsampleApplication>(*args)
+}
---
+++ src/test/kotlin/org/bbakaple/websocketsample/WebsocketsampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.bbakaple.websocketsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebsocketsampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
