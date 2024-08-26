d32dd62ae88bdb39748eca1f20475c54824d76f7
kevstevie <jianna442@gmail.com>
2024-08-26 19:18:58 +0900

    fix: requestbody 추가

--- src/main/kotlin/org/bbakaple/websocketsample/member/MemberController.kt
+++ src/main/kotlin/org/bbakaple/websocketsample/member/MemberController.kt
@@ -2,19 +2,20 @@ package org.bbakaple.websocketsample.member
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class MemberController(private val memberService: MemberService) {
 
     @PostMapping("/members")
-    fun create(request: MemberCreateRequest): ResponseEntity<Unit> {
+    fun create(@RequestBody request: MemberCreateRequest): ResponseEntity<Unit> {
         memberService.create(request)
         return ResponseEntity.ok().build()
     }
 
     @PostMapping("/login")
-    fun login(request: LoginRequest): ResponseEntity<Unit> {
+    fun login(@RequestBody request: LoginRequest): ResponseEntity<Unit> {
         memberService.login(request)
         return ResponseEntity.ok().build()
     }
