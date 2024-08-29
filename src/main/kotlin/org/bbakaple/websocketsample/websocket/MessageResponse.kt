d29a4090005c2eb949edd29bd23305a37cc5396a
kevstevie <jianna442@gmail.com>
2024-08-29 13:36:28 +0900

    style: data 중괄호제거

--- src/main/kotlin/org/bbakaple/websocketsample/websocket/MessageResponse.kt
+++ src/main/kotlin/org/bbakaple/websocketsample/websocket/MessageResponse.kt
@@ -2,5 +2,4 @@ package org.bbakaple.websocketsample.websocket
 
 import java.time.LocalDateTime
 
-data class MessageResponse(val writer: String, val text: String, val time: LocalDateTime) {
-}
+data class MessageResponse(val writer: String, val text: String, val time: LocalDateTime)
